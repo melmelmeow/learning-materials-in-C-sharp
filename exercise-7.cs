b6c0541de6c4f83449b6cc91d3c97907ed09ec9b
melmelmeow <lyndt.dayrit@gmail.com>
2026-01-15 02:06:27 +0800

    problem
    Implemented Bubble Sort algorithm to sort an array of integers.

---
+++ exercise-7.cs
@@ -0,0 +1,20 @@
+void BubbleSort(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        for (int j = 0; j < arr.Length - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int[] arr = { 5, 1, 4, 2 };
+BubbleSort(arr);
+
+Console.WriteLine(string.Join(", ", arr)); // Output: 1, 2, 4, 5
